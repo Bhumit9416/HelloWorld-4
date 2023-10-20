59cc3a93c3fbbbaa28fa78a9a617598b5f81d991
Bhumit Mittal <113707003+Bhumit9416@users.noreply.github.com>
2023-10-21 05:20:53 +0530

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define your global variables, structures, and functions here.
+
+// Function prototypes
+void function1();
+void function2();
+void function3();
+
+int main() {
+    // Your main program logic here
+
+    // Call your functions as needed
+    function1();
+    function2();
+    function3();
+
+    return 0;
+}
+
+// Define your functions here
+void function1() {
+    // Function 1 logic
+}
+
+void function2() {
+    // Function 2 logic
+}
+
+void function3() {
+    // Function 3 logic
+}
